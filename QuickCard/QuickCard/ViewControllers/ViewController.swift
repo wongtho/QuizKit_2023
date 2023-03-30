e1efaa26456109129da7f0a3a0d8c8a48949ae37
Aiden Janey <arjaney.professional@gmail.com>
2023-03-30 16:17:14 -0400

    Initial Commit

---
+++ QuickCard/QuickCard/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickCard
+//
+//  Created by  on 2023-03-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
