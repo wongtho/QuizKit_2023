8db9bd12736941381cb5317007f5fe937e585581
GauthamkV02 <127424114+GauthamkV02@users.noreply.github.com>
2023-04-19 00:16:41 -0400

    quiz
    Added quiz function and Result function

--- QuickCard/QuickCard/ViewControllers/ViewController.swift
+++ QuickCard/QuickCard/ViewControllers/ViewController.swift
@@ -11,10 +11,13 @@ class ViewController: UIViewController {
 
     @IBOutlet var StartButton : UIButton!
 
-    
+    @IBAction func unwindToAppViewController(sender : UIStoryboardSegue){
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         StartButton.layer.cornerRadius = 26
         StartButton.layer.masksToBounds = true
