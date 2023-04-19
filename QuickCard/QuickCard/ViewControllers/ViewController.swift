c9fc975fc02ad3610dbf5b1985fedf53f7a69b19
GauthamkV02 <127424114+GauthamkV02@users.noreply.github.com>
2023-04-18 22:38:22 -0400

    Start
    Added launch screen and open screen

--- QuickCard/QuickCard/ViewControllers/ViewController.swift
+++ QuickCard/QuickCard/ViewControllers/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var StartButton : UIButton!
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        StartButton.layer.cornerRadius = 26
+        StartButton.layer.masksToBounds = true
+
     }
 
-
 }
-
