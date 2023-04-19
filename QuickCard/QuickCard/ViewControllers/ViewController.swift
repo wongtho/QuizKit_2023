6dabda37967bd6754416475361612e44d749ec16
Thomas Wong <wongtho@sheridancollege.ca>
2023-04-19 18:41:35 -0400

    Added push notification

--- QuickCard/QuickCard/ViewControllers/ViewController.swift
+++ QuickCard/QuickCard/ViewControllers/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
@@ -19,6 +20,46 @@ class ViewController: UIViewController {
         StartButton.layer.cornerRadius = 26
         StartButton.layer.masksToBounds = true
 
+        //added push notifications 1 minute from launch
+        checkPushPermission()
     }
+    func checkPushPermission() {
+        let notifcationCenter = UNUserNotificationCenter.current()
+        notifcationCenter.getNotificationSettings { settings in
+            switch settings.authorizationStatus {
+                case .authorized:
+                    self.dispatchNotification()
+                case .denied:
+                    return
+                case .notDetermined:
+                notifcationCenter.requestAuthorization(options: [.alert, .sound]) {didAllow, error in
+                        if didAllow {
+                            self.dispatchNotification()
+                        }
+                    }
+                default:
+                    return
+                }
+        }
+    }
+    func dispatchNotification() {
+        let notificationCenter = UNUserNotificationCenter.current()
+        let title = "QuickCard started!"
+        let body = "Playing quiz"
 
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        content.sound = .default
+
+        let date = Date()
+        var dateComponent = DateComponents(calendar: Calendar.current, timeZone: TimeZone.current)
+        dateComponent.hour = Calendar.current.component(.hour, from: date)
+        dateComponent.minute = Calendar.current.component(.minute, from: date) + 1
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: false)
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+        notificationCenter.add(request)
+
+    }
 }
